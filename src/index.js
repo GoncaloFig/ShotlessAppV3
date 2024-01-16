2cc5efb279c15dac06fab01aa6cbd114bcc3c335
Gonçalo Figueiredo <goncalofigueiredowork@gmail.com>
2024-01-16 03:41:37 +0000

    updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, { hydrateRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,6 +10,9 @@ root.render(
     <App />
   </React.StrictMode>
 );
+// const app = document.getElementById('root');
+// const root = ReactDOM.hydrateRoot(app, <App suppressHydrationWarning/>);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
