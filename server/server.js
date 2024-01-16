f1ab79f196728c2ba27cc84a963cd16e954b5cd8
Gonçalo Figueiredo <goncalofigueiredowork@gmail.com>
2024-01-16 03:07:06 +0000

    updt pack

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.use('^/$', (req, res, next) => {
             return res.send(
                 data.replace(
                     '<div id="root"></div>', 
-                    `<div id="root">${renderToString(<App/>)}</div>`)
+                    `<div id="root">${renderToString(ReactDOMServer.renderToString(<App/>))}</div>`)
             )
         })
 
