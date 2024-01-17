ca71a086d39484172a6d2c0dbf3bdec89c7da448
Gonçalo Figueiredo <goncalofigueiredowork@gmail.com>
2024-01-17 22:20:59 +0000

    nm

--- src/server/server.js
+++ src/server/server.js
@@ -1,52 +1,42 @@
 import express from "express";
-import fs from 'fs'
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 import React from "react";
 import { renderToString } from 'react-dom/server';
 import App from '../App';
-import serverless from 'serverless-http'
+import serverless from 'serverless-http';
 
-const PORT = 4000;
 const app = express();
 
-const router = express.Router()
+app.use('/static', express.static(path.resolve(__dirname, '..', 'build', 'static')));
 
-// app.use('/static/css', express.static(path.resolve(__dirname, '..', 'build', 'static', 'css')));
-router.use('/static', express.static(path.resolve(__dirname, '..', 'build', 'static')));  //<- LAST FAILURE
-
-router.use('/*', (req, res, next) => {
+app.use('/*', (req, res) => {
     try {
         console.log('Reading index.html file:', path.resolve(__dirname, '..', 'build', 'index.html'));
         fs.readFile(path.resolve(__dirname, '..', 'build', 'index.html'), 'utf-8', (err, data) => {
-            if(err){
-                console.log(err)
-                return res.status(500).send("Some error happened")
+            if (err) {
+                console.log(err);
+                return res.status(500).send("Some error happened");
             }
 
             const appHtml = renderToString(<App/>);
 
             const htmlOutput = data.replace(
-                '<div id="root"></div>', 
+                '<div id="root"></div>',
                 `<div id="root">${appHtml}</div>
                 <script defer="defer" src="/static/js/main.js"></script>`
-            )
+            );
 
             console.log('Contents of index.html:', data);
             console.log('Rendered HTML App:', renderToString(<App/>));
 
-            return res.send(htmlOutput)
-        })
-
+            return res.send(htmlOutput);
+        });
     } catch (error) {
         console.error(error);
         return res.status(500).send('Internal Server Error');
     }
 });
 
-router.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
-
-router.use('/.netlify/src/server', router)
-
-module.exports.handler = serverless(app)
+// Export the Express app wrapped with serverless-http
+module.exports.handler = serverless(app);
